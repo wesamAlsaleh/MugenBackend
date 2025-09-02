e4120341b29e1a158d261641b13d917d7ec9ea21
wesamAlsaleh <wesammuneer7@gmail.com>
2025-09-02 16:42:14 +0300

    Initial commit

---
+++ src/main/java/com/avocadogroup/mugen/MugenApplication.java
@@ -0,0 +1,13 @@
+package com.avocadogroup.mugen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MugenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MugenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/avocadogroup/mugen/MugenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avocadogroup.mugen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MugenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
