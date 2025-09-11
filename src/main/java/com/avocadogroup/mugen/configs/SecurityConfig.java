49f2bae3c77d6ee61775968b44317160a973b354
wesamAlsaleh <wesammuneer7@gmail.com>
2025-09-11 10:20:09 +0300

    chore: update dependancies (install swagger)
    Note: a tool that helps to automate the generation of API documentation using spring boot projects

--- src/main/java/com/avocadogroup/mugen/configs/SecurityConfig.java
+++ src/main/java/com/avocadogroup/mugen/configs/SecurityConfig.java
@@ -52,6 +52,11 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.POST,"auth/verify-password-reset-otp").permitAll()
                         .requestMatchers(HttpMethod.POST,"auth/reset-password").permitAll()
                         .requestMatchers(HttpMethod.GET, "anime/**").permitAll()
+                // Swagger Endpoints (no authentication required)
+                        .requestMatchers("/swagger-ui/**").permitAll()
+                        .requestMatchers("/v3/api-docs/**").permitAll()
+                        .requestMatchers("/swagger-ui.html").permitAll()
+                        .requestMatchers("/webjars/**").permitAll() // Required for Swagger UI static files (CSS, JS)
                 // Role Based Endpoints (Requires specific role)
                         .requestMatchers("/admin/**").hasRole(UserRole.ADMIN.name()) // Only users with ADMIN role can access /admin/**
                 // All other endpoints (authentication token required)
